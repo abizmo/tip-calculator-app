48321a77014c49a1da51c05c4298229cd7011989
abizmo <abizmo@gmail.com>
2021-12-01 19:16:15 +0000

    feat(tipcalculator): add tipcalculator's container

--- src/components/TipCalculator/index.jsx
+++ src/components/TipCalculator/index.jsx
@@ -1,17 +1,36 @@
+import React from 'react';
+
 import styled from 'styled-components';
 
-const TipCalculator = styled.div`
+const Container = styled.div`
   background-color: ${({ theme }) => theme.color.neutral.white};
   border-radius: 16px 16px 0 0;
+  display: flex;
+  flex-direction: column;
+  gap: 32px;
   height: 500px;
-  width: 100%;
   max-width: 375px;
+  padding: 32px;
+  width: 100%;
 
   @media(min-width: 1024px) {
     border-radius: 16px;
     box-shadow: rgba(0, 0, 0, 0.1) 0px 20px 25px -5px, rgba(0, 0, 0, 0.04) 0px 10px 10px -5px;
+    flex-direction: row;
     max-width: 920px;
   }
 `;
+
+const Div = styled.div`
+  background-color: blanchedalmond;
+  flex: 1;
+`;
+
+const TipCalculator = () => (
+  <Container>
+    <Div />
+    <Div />
+  </Container>
+);
 
 export default TipCalculator;
